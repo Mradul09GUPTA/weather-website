1885e4252d93016760ef43470dc881f12ed32c6a
Mradul09GUPTA <106403838+Mradul09GUPTA@users.noreply.github.com>
2022-09-07 18:40:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const timezone = document.getElementById('time-zone');
 const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
+const searchInput=document.getElementById('search_input') ;
+const searchButton=document.getElementById('search-button')
+const background= document.body;
 
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
@@ -25,7 +28,11 @@ setInterval(() => {
     timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
 
     dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
-
+    const images=hour > 19 ? "url('https://cdn.pixabay.com/photo/2015/02/24/15/41/wolf-647528__480.jpg')" :"url('https://images.unsplash.com/photo-1621274403997-37aace184f49?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1')";
+    
+    background.style.background=images;
+    background.style.backgroundRepeat="no-repeat";
+    background.style.backgroundSize="cover";
 }, 1000);
 
 getWeatherData()
@@ -33,6 +40,7 @@ function getWeatherData () {
     navigator.geolocation.getCurrentPosition((success) => {
         
         let {latitude, longitude } = success.coords;
+        
 
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
 
@@ -43,14 +51,45 @@ function getWeatherData () {
     })
 }
 
-function showWeatherData (data){
-    let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+console.log(searchInput.value);
+searchButton.addEventListener('click',(e)=>{
+    e.preventDefault();
+    //console.log(searchInput.value);
+    getWeather(searchInput.value);
+    searchInput.value="";
 
-    timezone.innerHTML = data.timezone;
+
+})
+function getWeather(city){
+         console.log(city);
+         const cnt=7;
+         const lat="";
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&exclude=hourly,minutely&units=metric&cnt=${cnt}&appid=dab3af44de7d24ae7ff86549334e45bd`).then(res => res.json()).then(qwe=>{
+            console.log(qwe);
+            let{lon,lat}=qwe.coord;
+            timezone.innerHTML=qwe.name;
+            console.log(lon+" "+lat);
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(wer => {
+
+        console.log(wer)
+        showWeatherData(wer);
+        })
+        })
+    }
+function showWeatherData (data){
+    let {humidity, pressure, sunrise, sunset, wind_speed,temp} = data.current;
+
+    //timezone.innerHTML = data.timezone;
     countryEl.innerHTML = data.lat + 'N ' + data.lon+'E'
 
     currentWeatherItemsEl.innerHTML = 
-    `<div class="weather-item">
+    `
+    <div class="weather-item">
+    <div>Temp</div>
+    <div>${temp} &#176C</div>
+</div>
+    <div class="weather-item">
         <div>Humidity</div>
         <div>${humidity}%</div>
     </div>
@@ -62,7 +101,6 @@ function showWeatherData (data){
         <div>Wind Speed</div>
         <div>${wind_speed}</div>
     </div>
-
     <div class="weather-item">
         <div>Sunrise</div>
         <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div>
@@ -70,11 +108,7 @@ function showWeatherData (data){
     <div class="weather-item">
         <div>Sunset</div>
         <div>${window.moment(sunset*1000).format('HH:mm a')}</div>
-    </div>
-    
-    
-    `;
-
+    </div> `;
     let otherDayForcast = ''
     data.daily.forEach((day, idx) => {
         if(idx == 0){
@@ -102,4 +136,5 @@ function showWeatherData (data){
 
 
     weatherForecastEl.innerHTML = otherDayForcast;
+    
 }
